6234abac0fea6a62390929e056853dc8e947c8cb
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-01-10 20:18:06 +0530

    address copy update

--- src/app/components/currentProjects.jsx
+++ src/app/components/currentProjects.jsx
@@ -3,13 +3,14 @@ import AnimatedText from './AnimatedText'
 
 function CurrentProjects() {
   return (
-    <div className='p-4'>
-        <AnimatedText text="Current Projects" time="0.5" className="font-cyberalert text-4xl text-yellow-500" customText="日本語組版処理" />
-        <ul>
-          <AnimatedText text="Project 1" time="1" className="font-helvetica text-2xl text-white" customText="12345" preStyle='font-helvetica text-2xl bg-yellow-500 text-black'/>
-          <AnimatedText text="Project 2" time="1" className="font-helvetica text-2xl text-white" customText="12345" preStyle='font-helvetica text-2xl bg-yellow-500 text-black'/>
-          <AnimatedText text="Project 3" time="1" className="font-helvetica text-2xl text-white" customText="12345" preStyle='font-helvetica text-2xl bg-yellow-500 text-black'/>
-          <AnimatedText text="Project 4" time="1" className="font-helvetica text-2xl text-white" customText="12345" preStyle='font-helvetica text-2xl bg-yellow-500 text-black'/>
+    <div className='m-4 w-1/2 flex flex-col items-center'>
+        <AnimatedText text="Current Projects" time="0.5" className="py-4 font-cyberalert text-5xl text-yellow-500" customText="日本語組版処理" />
+        <ul className='p-4 w-full'>
+          <li className='flex justify-between w-full'>
+              <AnimatedText text="Project 1" time="1" className="font-helvetica text-3xl text-yellow-500" customText="12345" preStyle='font-helvetica text-3xl bg-yellow-500 text-black'/>
+              
+              <AnimatedText text="Address 1" time="1" className="font-helvetica text-3xl text-white" customText="12345" preStyle='font-helvetica text-3xl bg-yellow-500 text-black'/>
+          </li>
         </ul> 
     </div>
   )
--- src/app/dashboard/Waddress.jsx
+++ src/app/dashboard/Waddress.jsx
@@ -3,7 +3,15 @@ import AnimatedText from '../components/AnimatedText'
 import Image from 'next/image'
 
 function Waddress() {
-  return (
+
+    const address = "0x1234567890";
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(address);
+        alert("Address copied to clipboard!");
+    };
+
+    return (
     <div className="flex w-full justify-between py-4">
         <Image
           src="/images/hud/Untitled-1_06.jpg"
@@ -14,7 +22,9 @@ function Waddress() {
 
         <div className="flex flex-col items-center justify-between">
           <AnimatedText text="WALLET ADDRESS" time="0.5" className="font-cyberalert text-5xl text-yellow-500" customText="日本語組版処理" />
-          <AnimatedText text="0x1234567890" time="0.5" className="text-3xl font-cyberalert" customText="x?▂▄▆█" preStyle='text-3xl font-cyberalert text-yellow-300' />
+          <div onClick={handleCopy} className='cursor-pointer'>
+              <AnimatedText text={address} time="0.5" className="text-3xl font-cyberalert" customText="x?▂▄▆█" preStyle='text-3xl font-cyberalert text-yellow-300' />
+          </div>
         </div>
         <Image
           src="/images/hud/Untitled-1_14.jpg"
